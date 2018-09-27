a24b9839a3f2fe35b26e8fe85b5e7ba395acbfdc
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-09-27 16:26:35 -0500

    Added the index.php

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
-<?php include("includes/a_config.php");?>
 <!DOCTYPE html>
 <html>
 <head>
 </head>
 <body>
 
-<?php include("design-top.php");?>
-<?php include("navigation.php");?>
-
+<?php include("frontend/design-top.php");?>
+<?php include("frontend/navigation.php");?>
+<h1><center>Hello World!</center></h1>
 <div class="container" id="main-content">
 	<h2>Welcome to the website!</h2>
 	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
 </div>
 
-<?php include("footer.php");?>
+<?php include("frontend/footer.php");?>
 
 </body>
 </html>
