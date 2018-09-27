1f927d6e64a0c069ed5975e65798898bef556bc7
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-09-27 16:10:38 -0500

    Added the other homepage php files - Navigation, footer, design-top

---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php include("design-top.php");?>
+<?php include("navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>Welcome to the website!</h2>
+	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
+</div>
+
+<?php include("footer.php");?>
+
+</body>
+</html>
