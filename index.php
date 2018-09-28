6f86b636a1ecf2800a89a3074da4aa62dd96ca9c
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-09-27 23:13:07 -0500

    Changed title to check crontab setting

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <?php include("frontend/navigation.php");?>
 <h1><center>Hello World!</center></h1>
 <div class="container" id="main-content">
-	<h2>Welcome to the website!</h2>
+	<h2>Welcome to the gaming website!</h2>
 	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
 </div>
 
