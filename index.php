b3deaedac63534b5a9d87682926e9055e7cb584e
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-09-28 00:12:35 -0500

    final test for cronjob

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <?php include("frontend/design-top.php");?>
 <?php include("frontend/navigation.php");?>
-<h1><center>Hello World!</center></h1>
+<h1><center>Hello World!!!</center></h1>
 <div class="container" id="main-content">
 	<h2>Welcome to the gaming store!</h2>
 	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
