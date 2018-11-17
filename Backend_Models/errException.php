cfb16afb1137fbc580784112438300502ea5b1fa
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-11-17 16:27:58 -0600

    added the errException.php

---
+++ Backend_Models/errException.php
@@ -0,0 +1,16 @@
+<?php
+
+// Model to handle all types of exceptions 
+
+class errExceptions extends Exception
+{
+	private $errMap;
+	private $errPresent;
+
+	function __contruct()
+	{
+		
+	}
+}
+
+?>
