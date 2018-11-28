dd9b342872add51b76b08f34b376c5d220a721ae
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-11-27 20:40:19 -0600

    now added the logout button and removed the drop down

--- index.php
+++ index.php
@@ -66,6 +66,7 @@
     }
     ?>
   	<a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
+    <!-->
     <div class="w3-dropdown-hover w3-hide-small">
     <button class="w3-button" title="Notifications">Dropdown <i class="fa fa-caret-down"></i></button>     
     <div class="w3-dropdown-content w3-card-4 w3-bar-block">
@@ -73,8 +74,9 @@
       <a href="#" class="w3-bar-item w3-button">Link</a>
       <a href="#" class="w3-bar-item w3-button">Link</a>
     </div>
+    <!-->
   </div>
-  <a href="Frontend_Models/logout.php" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Logout">
+  <a href="Frontend_Models/logout.php" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Logout">Logout</a>
  </div>
 
   <!-- Navbar on small screens -->
