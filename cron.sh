998a835007f1449930788acfb6417120314f7015
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-11-28 12:14:27 -0600

    updated cron.sh

--- cron.sh
+++ cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /home/an839/public_html/DCSP/DCSP_Project/
+git co master
 git reset --hard HEAD
 LOCAL=git rev-parse HEAD
 REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/DCSP_Project.git HEAD
