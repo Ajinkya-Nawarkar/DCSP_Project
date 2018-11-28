88501bc449f000c4b56d660d6f9674b991662918
Aaron Valenti <aaronval47@google.com>
2018-11-28 14:06:23 -0600

    footer update

--- Frontend_Models/footer.php
+++ Frontend_Models/footer.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
+<title>Maroon Gaming</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
   
   <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
