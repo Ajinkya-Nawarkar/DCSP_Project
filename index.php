9cac2c5092f9e78ab435c5c6b67e7cc3027027bd
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-11-27 20:36:42 -0600

    added the maroon gaming text, added logout and shifted search

--- index.php
+++ index.php
@@ -36,10 +36,10 @@
         if(isset($_SESSION['type'])){
         switch ($_SESSION['type']) {
           case 'user':
-            echo " - Basic User";
+            echo "Maroon Gaming - Basic User";
             break;
           case 'admin':
-            echo " - Administrator";
+            echo "Maroon Gaming - Administrator";
             break;
         }
       }
@@ -65,7 +65,7 @@
       echo "<a href='Frontend_Models/login.php' class='w3-bar-item w3-button w3-hide-small w3-hover-white'>Login</a>";
     }
     ?>
-  	<a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
+  	<a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
     <div class="w3-dropdown-hover w3-hide-small">
     <button class="w3-button" title="Notifications">Dropdown <i class="fa fa-caret-down"></i></button>     
     <div class="w3-dropdown-content w3-card-4 w3-bar-block">
