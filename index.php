8148881bcf1b9a6b59eaaab499a363c5106e13ba
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-11-27 20:43:59 -0600

    added logout glyphicon

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
     }
     ?>
   	<a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
-    <!-->
+    <!-- >
     <div class="w3-dropdown-hover w3-hide-small">
     <button class="w3-button" title="Notifications">Dropdown <i class="fa fa-caret-down"></i></button>     
     <div class="w3-dropdown-content w3-card-4 w3-bar-block">
@@ -76,7 +76,8 @@
     </div>
     <!-->
   </div>
-  <a href="Frontend_Models/logout.php" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Logout">Logout</a>
+  <a href="Frontend_Models/logout.php" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Logout"><span class="glyphicon glyphicon-log-in"></span> Logout</a>
+
  </div>
 
   <!-- Navbar on small screens -->
