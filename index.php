8734d5c66ce7726be3d5a3c79f86943a35a290de
Ajinkya-Nawarkar <ajinkyanawarkar35@gmail.com>
2018-11-27 20:58:20 -0600

    added logout button on right place and only if loggedin

--- index.php
+++ index.php
@@ -75,9 +75,12 @@
       <a href="#" class="w3-bar-item w3-button">Link</a>
     </div>
     <!-->
+    	<?php
+  	if(isset($_SESSION['type'])){
+  		echo "<a href='Frontend_Models/logout.php' class='w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal' title='Logout'><i class='fa fa-sign-in' aria-hidden='true></i> Logout</a>"
+  	}
+  	?>
   </div>
-  <a href="Frontend_Models/logout.php" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Logout"><i class="fa fa-sign-in" aria-hidden="true"></i> Logout</a>
-
  </div>
 
   <!-- Navbar on small screens -->
