90f8919049043d6222211628ac15ea7833de160f
Aaron Valenti <aaronval47@google.com>
2018-11-29 17:38:38 -0600

    Test

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <body id="myPage">
 
-<!-- Sidebar on click 
+<!-- Sidebar on click
 <nav class="w3-sidebar w3-bar-block w3-white w3-card w3-animate-left w3-xxlarge" style="display:none;z-index:2" id="mySidebar">
   <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-display-topright w3-text-teal">Close
     <i class="fa fa-remove"></i>
@@ -46,9 +46,9 @@
         echo "";
       }
       ?></a>
-  
+
   <a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Home</a>
-  
+
   <?php
   	if(isset($_SESSION['type'])){
       switch ($_SESSION['type']) {
@@ -60,6 +60,8 @@
           break;
       }
     }
+
+
     else{
       echo "<a href='Frontend_Models/login.php' class='w3-bar-item w3-button w3-hide-small w3-hover-white'><i class='fa fa-sign-in' aria-hidden='true'></i> Login</a>";
     }
@@ -68,7 +70,7 @@
   	<a href="index.php" class="w3-bar-item w3-button w3-hide-small w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
     <!-- >
     <div class="w3-dropdown-hover w3-hide-small">
-    <button class="w3-button" title="Notifications">Dropdown <i class="fa fa-caret-down"></i></button>     
+    <button class="w3-button" title="Notifications">Dropdown <i class="fa fa-caret-down"></i></button>
     <div class="w3-dropdown-content w3-card-4 w3-bar-block">
       <a href="#" class="w3-bar-item w3-button">Link</a>
       <a href="#" class="w3-bar-item w3-button">Link</a>
@@ -102,11 +104,11 @@
   <img src="Assets/banner.png" alt="boat" style="width:100%;min-height:350px;max-height:600px;">
 </div>
 
-<!-- Modal 
+<!-- Modal
 
 <div id="id01" class="w3-modal">
   <div class="w3-modal-content w3-card-4 w3-animate-top">
-    <header class="w3-container w3-teal w3-display-container"> 
+    <header class="w3-container w3-teal w3-display-container">
       <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-teal w3-display-topright"><i class="fa fa-remove"></i></span>
       <h4>Oh snap! We just showed you a modal..</h4>
       <h5>Because we can <i class="fa fa-smile-o"></i></h5>
@@ -131,7 +133,7 @@
 <div class="w3-card w3-white">
   <!--<img src="/w3images/snow.jpg" alt="Snow" style="width:100%"><-->
   <div class="w3-container">
-  <h3>Product .5</h3>
+  <h3>Product 1241421</h3>
   <h4>Trade</h4>
   <p>Blablabla</p>
   <p>Blablabla</p>
@@ -157,7 +159,7 @@
 
 <div class="w3-quarter">
 <div class="w3-card w3-white">
-  
+
   <div class="w3-container">
   <h3>product2 2</h3>
   <h4>Trade</h4>
@@ -185,7 +187,7 @@
 
 </div>
 
-<!-- 
+<!--
 <div class="w3-container" style="position:relative">
   <a onclick="w3_open()" class="w3-button w3-xlarge w3-circle w3-teal"
   style="position:absolute;top:-28px;right:24px">+</a>
@@ -262,27 +264,27 @@
 <div class="w3-row"><br>
 
 <div class="w3-quarter">
-  
+
   <h3>Johnny Walker</h3>
-  
+
 </div>
 
 <div class="w3-quarter">
-  
+
   <h3>Rebecca Flex</h3>
-  
+
 </div>
 
 <div class="w3-quarter">
-  
+
   <h3>Jan Ringo</h3>
-  
+
 </div>
 
 <div class="w3-quarter">
-  
+
   <h3>Kai Ringo</h3>
-  
+
 </div>
 
 </div>
@@ -300,18 +302,18 @@
     </div>
     <div class="w3-col m7">
       <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="/action_page.php" target="_blank">
-      <div class="w3-section">      
+      <div class="w3-section">
         <label>Name</label>
         <input class="w3-input" type="text" name="Name" required>
       </div>
-      <div class="w3-section">      
+      <div class="w3-section">
         <label>Email</label>
         <input class="w3-input" type="text" name="Email" required>
       </div>
-      <div class="w3-section">      
+      <div class="w3-section">
         <label>Message</label>
         <input class="w3-input" type="text" name="Message" required>
-      </div>  
+      </div>
       <input class="w3-check" type="checkbox" checked name="Like">
       <label>I Like it!</label>
       <button type="submit" class="w3-button w3-right w3-theme">Send</button>
@@ -321,7 +323,7 @@
 </div>
 
 
-<!-- Image of location/map 
+<!-- Image of location/map
 <img src="/w3images/map.jpg" class="w3-image w3-greyscale-min" style="width:100%;">
 -->
 <!-- Footer -->
@@ -332,11 +334,11 @@
   <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-google-plus"></i></a>
   <a class="w3-button w3-large w3-teal" href="javascript:void(0)" title="Google +"><i class="fa fa-instagram"></i></a>
   <a class="w3-button w3-large w3-teal w3-hide-small" href="javascript:void(0)" title="Linkedin"><i class="fa fa-linkedin"></i></a>
-  
+
   <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
 
   <div style="position:relative;bottom:100px;z-index:1;" class="w3-tooltip w3-right">
-    <span class="w3-text w3-padding w3-teal w3-hide-small">Go To Top</span>   
+    <span class="w3-text w3-padding w3-teal w3-hide-small">Go To Top</span>
     <a class="w3-button w3-theme" href="#myPage"><span class="w3-xlarge">
     <i class="fa fa-chevron-circle-up"></i></span></a>
   </div>
@@ -361,7 +363,7 @@ function openNav() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
-    } else { 
+    } else {
         x.className = x.className.replace(" w3-show", "");
     }
 }
