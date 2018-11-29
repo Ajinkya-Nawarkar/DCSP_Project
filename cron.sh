faf3f7e2ca3abe06e4e46b56cfb36dceaab548d7
Aaron Valenti <aaronval47@google.com>
2018-11-29 17:43:42 -0600

    cron

--- cron.sh
+++ cron.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd /home/an839/public_html/DCSP/DCSP_Project/
+git checkout index_front
 git reset --hard HEAD
 LOCAL=git rev-parse HEAD
 REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/DCSP_Project.git HEAD
 
 if ["$LOCAL" != "$REMOTE"]
 then
-    git pull
-    git checkout index_front
+    git pull origin index_front
 fi
 
 chmod -R 777 .
