26f28fb6f2b8bd5b207c2529712eb2e73b430827
Aaron Valenti <aaronval47@google.com>
2018-11-28 19:02:41 -0600

    Correct comments 1

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 
-
-
 <?php session_start(); error_reporting(E_ALL); ini_set('display_errors', 1); ?>
 
 <!DOCTYPE html>
@@ -13,7 +11,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <body id="myPage">
 
-<!-- Sidebar on click -->
+<!-- Sidebar on click 
 <nav class="w3-sidebar w3-bar-block w3-white w3-card w3-animate-left w3-xxlarge" style="display:none;z-index:2" id="mySidebar">
   <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-display-topright w3-text-teal">Close
     <i class="fa fa-remove"></i>
@@ -24,6 +22,8 @@
   <a href="#" class="w3-bar-item w3-button">Link 4</a>
   <a href="#" class="w3-bar-item w3-button">Link 5</a>
 </nav>
+
+-->
 
 <!-- Navbar -->
 <div class="w3-top">
@@ -101,8 +101,8 @@
   <img src="Assets/banner.png" alt="boat" style="width:100%;min-height:350px;max-height:600px;">
 </div>
 
-<!-- Modal -->
-<!-->
+<!-- Modal 
+
 <div id="id01" class="w3-modal">
   <div class="w3-modal-content w3-card-4 w3-animate-top">
     <header class="w3-container w3-teal w3-display-container"> 
@@ -119,43 +119,11 @@
     </footer>
   </div>
 </div>
+-->
 
-<!-- Team Container -->
-<!-->
-<div class="w3-container w3-padding-64 w3-center" id="team">
-<h2>OUR TEAM</h2>
-<p>Meet the team - our office rats:</p>
 
-<div class="w3-row"><br>
 
-<div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Johnny Walker</h3>
-  <p>Web Designer</p>
-</div>
 
-<div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Rebecca Flex</h3>
-  <p>Support</p>
-</div>
-
-<div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Jan Ringo</h3>
-  <p>Boss man</p>
-</div>
-
-<div class="w3-quarter">
-  <img src="/w3images/avatar.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
-  <h3>Kai Ringo</h3>
-  <p>Fixer</p>
-</div>
-
-</div>
-</div>
-
-<!-- Work Row -->
 <div class="w3-row-padding w3-padding-64 w3-theme-l1" id="work">
 
 <div class="w3-quarter">
@@ -207,8 +175,7 @@
 
 </div>
 
-<!-- Container -->
-<!-->
+<!-- 
 <div class="w3-container" style="position:relative">
   <a onclick="w3_open()" class="w3-button w3-xlarge w3-circle w3-teal"
   style="position:absolute;top:-28px;right:24px">+</a>
@@ -275,8 +242,9 @@
       </ul>
     </div>
 </div>
+-->
 
-
+<!--
 <div class="w3-container w3-padding-64 w3-theme-l5" id="contact">
   <div class="w3-row">
     <div class="w3-col m5">
@@ -308,7 +276,42 @@
     </div>
   </div>
 </div>
-<!-->
+-->
+
+<!-- Team Container -->
+
+<div class="w3-container w3-padding-64 w3-center" id="team">
+<h2>OUR TEAM</h2>
+<p>Meet the team - our office rats:</p>
+
+<div class="w3-row"><br>
+
+<div class="w3-quarter">
+  
+  <h3>Johnny Walker</h3>
+  
+</div>
+
+<div class="w3-quarter">
+  
+  <h3>Rebecca Flex</h3>
+  
+</div>
+
+<div class="w3-quarter">
+  
+  <h3>Jan Ringo</h3>
+  
+</div>
+
+<div class="w3-quarter">
+  
+  <h3>Kai Ringo</h3>
+  
+</div>
+
+</div>
+</div>
 <!-- Image of location/map -->
 <img src="/w3images/map.jpg" class="w3-image w3-greyscale-min" style="width:100%;">
 
