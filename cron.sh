8f316c3eabd3d406ccda8f4034ed51b0459e490c
Ajinkya Nawarkar <ajinkyanawarkar35@gmail.com>
2018-12-04 13:15:19 -0600

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/an839/public_html/DCSP/link5_DCSP/DCSP_Project/
+cd /home/an839/public_html/DCSP/link3_DCSP/DCSP_Project/
 
 git reset --hard HEAD
 LOCAL=git rev-parse HEAD
@@ -8,9 +8,9 @@ REMOTE=git ls-remote https://github.com/Ajinkya-Nawarkar/DCSP_Project.git HEAD
 
 if ["$LOCAL" != "$REMOTE"]
 then
-    git checkout contact_dev
+    git checkout master
     git checkout .
-    git pull origin contact_dev
+    git pull origin master
 fi
 
 chmod -R 777 .
