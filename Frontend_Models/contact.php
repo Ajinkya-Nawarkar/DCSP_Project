055e2fad96e6f7b698458ebf4f0337ed29da9782
ChandlerU <43624791+ChandlerU@users.noreply.github.com>
2018-12-04 12:27:39 -0600

    Fix

--- Frontend_Models/contact.php
+++ Frontend_Models/contact.php
@@ -29,7 +29,7 @@
             }
             footer{
                 margin-top: 0px;
-                padding-bottom: 200px;
+                padding-bottom: 0px;
             }
             body{font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;font-size:14px;line-height:1.42857143;color:#333;background-color:#fff}
             input[type=text], input[type=password] {
@@ -110,15 +110,15 @@
       <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="index.php" target="_blank">
       <div class="w3-section">
         <label>Name</label>
-        <input class="w3-input" type="text" name="Name" required>
+        <input text-align:right; class="w3-input" type="text" name="Name" required>
       </div>
       <div class="w3-section">
         <label>Email</label>
-        <input margin_left: 20px; class="w3-input" type="text" name="Email" required>
+        <input text-align:right; class="w3-input" type="text" name="Email" required>
       </div>
       <div class="w3-section">
         <label>Message </label>
-        <input class="w3-input" type="text" name="Message" required>
+        <input text-align:right; class="w3-input" type="text" name="Message" required>
       </div>
       <input class="w3-check" type="checkbox" checked name="Like">
       <label>I Like it!</label>
