8b545cb1800caeb89b61d2c059ca4949879df7e8
ChandlerU <43624791+ChandlerU@users.noreply.github.com>
2018-12-03 20:12:20 -0600

    blah

--- Frontend_Models/contact.php
+++ Frontend_Models/contact.php
@@ -1,102 +1,112 @@
-
-<?php session_start(); error_reporting(E_ALL); ini_set('display_errors', 1); ?>
+<?php session_start(); error_reporting(E_ALL); ini_set('display_errors', 0); ?>
 
 <!DOCTYPE html>
-<html>
-<title>Maroon Gaming</title>
-<style>
-* {
-    box-sizing: border-box;
-}
-form.example input[type=text] {
-    padding: 10px;
-    font-size: 17px;
-    border: 1px solid grey;
-    float: left;
-    width: 80%;
-    background: #f1f1f1;
-}
-form.example button {
-    float: left;
-    width: 20%;
-    padding: 10px;
-    background: #800000;
-    color: white;
-    font-size: 17px;
-    border: 1px solid grey;
-    border-left: none;
-    cursor: pointer;
-}
-form.example button:hover {
-    background: #D3D3D3;
-}
-form.example::after {
-    content: "";
-    clear: both;
-    display: table;
-}
-#myInput {
-  background-image: url('/css/searchicon.png');
-  background-position: 10px 10px;
-  background-repeat: no-repeat;
-  width: 100%;
-  font-size: 16px;
-  padding: 12px 20px 12px 40px;
-  border: 1px solid #ddd;
-  margin-bottom: 12px;
-}
-* {box-sizing: border-box;}
-body {font-family: Verdana, sans-serif;}
-.mySlides {display: none;}
-img {vertical-align: middle;}
-/* Slideshow container
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
-*/
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-/* The dots/bullets/indicators */
-.dot {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-.active {
-  background-color: #717171;
-}
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-@-webkit-keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
-</style>
+<html lang='en'>
+    <head>
+        <link href="css.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <meta charset="UTF-8">
+        <title>Maroon Gaming Co</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <style>
+            #login{
+                margin-top: 80px;
+            }
+            .text-center{
+            margin-top: -10px;
+            }
+            .form-group{
+                margin-top: 5px;
+                padding-bottom: 3px;
+            }
+            .form-group-submit{
+                margin-top: 14px;
+                padding-bottom: 2px;
+            }
+            footer{
+                margin-top: 160px;
+                padding-bottom: 200px;
+            }
+            body{font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;font-size:14px;line-height:1.42857143;color:#333;background-color:#fff}
+            input[type=text], input[type=password] {
+                width: 25%;
+                padding: 15px;
+                margin: 5px 0 5px 0;
+                display: inline-block;
+                border: none;
+                background: #f1f1f1;
+            }
+            input[type=text]:focus, input[type=password]:focus {
+                background-color: #ddd;
+                outline: none;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+            require_once(dirname(__DIR__)."/Backend_Models/Common.php");
+            require_once(dirname(__DIR__)."/Database/dbAPI.php");
+            // Initialize the variables and object for Common.php
+            $error_string = NULL;
+            $admin_check = "";
+            $type = "";
+            $common = new Common;
+            if (isset($_SESSION['type']))
+                redirectUser();
+            
+            // Preserve the username to show if only password is incorrect
+            $username = isset($_POST['username']);
+            
+            if (isset($_POST['username']) && isset($_POST['password'])) 
+            {
+                // Initialize the object for Database.php
+                $database = new dbAPI;
+                $un_temp = mysql_entities_fix_string($database->connection, $_POST['username']);
+                $pw_temp = mysql_entities_fix_string($database->connection, $_POST['password']);
+                // admin_check validation
+                if(isset($_POST['admin_check']))
+                {
+                    $result = $database->getOneAdmin($un_temp);
+                    $admin_check = "checked";
+                    $type = "admin";
+                }
+                else{
+                    $result = $database->getOneUser($un_temp);   
+                    $type = "user";
+                }
+            
+                // Validate the password and set session variables
+                if ($result)
+                {
+                    $token = $common->hashPassword($pw_temp);
+                    if ($token == $result[1])
+                    {
+                        $error_string = NULL;
+                        $common->setSession($un_temp, $type);
+                        redirectUser();
+                    }
+                    else
+                        $error_string = "Your username/password combination is incorrect. Try Again!";
+                }
+                else 
+                    $error_string = "Your username/password combination is incorrect. Try Again!";
+            }
+                     
+        ?>
+
+    <div class="w3-top">
+        <div class="w3-bar w3-theme-d2 w3-left-align">
+
+            <a href="#" class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>Maroon Gamer</a>
+            <a href="../index.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Home</a>
+
+            <a href='signup.php' class='w3-bar-item w3-button w3-hide-small w3-right w3-teal' title='Signup'><i class='fa fa-sign-in' aria-hidden='true'></i>  Signup</a>
+        </div>
+    </div>
     <body>
 <php
   <div class="w3-container w3-padding-64 w3-theme-l5" id="contact">
