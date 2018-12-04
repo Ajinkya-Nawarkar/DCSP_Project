4c619c9836b30c3700b88a2da848681b9f319c89
JackBetbeze <jbetbeze47@gmail.com>
2018-12-03 18:13:23 -0600

    edited cart.phps errexception include; changed view_cart.php to viewCart.php; edited redirectuser

---
+++ Backend_Models/cart.php
@@ -0,0 +1,54 @@
+<?php
+require_once(dirname(__DIR__)."/Backend_Models/errException.php");
+require_once(dirname(__DIR__)."/Database/dbAPI.php");
+
+class Cart
+{
+  private $username;
+  private $database;
+
+  function __construct($un)
+  {
+    $this->username = $un;
+    $this->database = new dbAPI;
+  }
+
+  function getItems()
+  {
+    $items = $this->database->query("SELECT cart FROM users WHERE username='$this->username'");
+    $cart = array();
+    for ($int i = 0; $i < sizeof($items); $i++) {
+      if ($items[$i] != '0') {
+        $cart[$i] = $items[$i];
+      }
+    }
+    return $cart;
+  }
+
+  function getItemDetails($skus)
+  {
+    $items = array();
+    foreach ($skus as $sku => $quant) {
+      $curr_item = $this->database->getItem($sku);
+      array_push($items, $curr_item);
+    }
+    return $items;
+  }
+
+    return $result;
+  }
+
+  function addToCart($sku, $amount)
+  {
+    $this->database->addToCart($this->username, $sku, $amount);
+  }
+
+  function removeFromCart($sku) {
+    $this->database->removeFromCart($this->username, $sku);
+  }
+
+  function editCartQuant($sku, $amount) {
+    $this->database->editCartQuant($this->username, $sku, $amount);
+  }
+}
+?>
