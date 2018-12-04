82d7e96c6318fda20ccb067f148193ac3c924ee1
JackBetbeze <jbetbeze47@gmail.com>
2018-12-03 18:54:27 -0600

    more debugging

--- Frontend_Models/viewCart.php
+++ Frontend_Models/viewCart.php
@@ -137,10 +137,10 @@ session_start();
                         </colgroup>
                         <tr><th class="text-info"><b>SKU</b></th><th class="text-info"><b>Name</b></th><th class="text-info"><b>Price</b></th><th class="text-info"><b>Quantity</b></th><th class="text-info"><b>Remove</b></th></tr>
                         <?php
-                        for ($i = 0; $i < sizeof($cart_all); $i++) {
+                        /*for ($i = 0; $i < sizeof($cart_all); $i++) {
                           echo "<tr><td>".$cart_all[$i]['sku']."</td><td>".$cart_all[$i]['name']."</td><td>".$cart_all[$i]['priceUSD']."</td>";
                           echo "<td><input type='number' name='quant".$cart_all[$i]['sku']."' min='0' max='".$cart_all[$i]['quantity']."' value='".$cart_quants[$cart_all[$i]['sku']]."' required></td>";
-                          echo "<td><input type='checkbox' name='remove".$cart_all[$i]['sku']."'></td></tr>";
+                          echo "<td><input type='checkbox' name='remove".$cart_all[$i]['sku']."'></td></tr>";*/
                         }
                         ?>
                         <tr style="background-color:#ffffff"><td></td><td></td><td></td><td></td><td style="vertical-align:middle"><input style="background-color: #4CAF50; color: white" type='submit' name='update' value='Update'></td></tr>
