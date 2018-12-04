52b68a3ec454f4548024371943e47ade6deb5836
ChandlerU <43624791+ChandlerU@users.noreply.github.com>
2018-12-04 08:27:09 -0600

    Added footer and formated input boxes

--- Frontend_Models/contact.php
+++ Frontend_Models/contact.php
@@ -110,15 +110,15 @@
       <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="index.php" target="_blank">
       <div class="w3-section">
         <label>Name</label>
-        <input class="w3-input" type="text" name="Name" required>
+        <input class="w3-input w3-right" type="text" name="Name" required>
       </div>
       <div class="w3-section">
         <label>Email</label>
-        <input class="w3-input" type="text" name="Email" required>
+        <input class="w3-input w3-right" type="text" name="Email" required>
       </div>
       <div class="w3-section">
         <label>Message</label>
-        <input class="w3-input" type="text" name="Message" required>
+        <input class="w3-input w3-right" type="text" name="Message" required>
       </div>
       <input class="w3-check" type="checkbox" checked name="Like">
       <label>I Like it!</label>
@@ -141,5 +141,8 @@
             <a href='signup.php' class='w3-bar-item w3-button w3-hide-small w3-right w3-teal' title='Signup'><i class='fa fa-sign-in' aria-hidden='true'></i>  Signup</a>
         </div>
     </div>
+     <footer class="w3-container w3-padding-32 w3-theme-d1 w3-center">
+    <h4>Maroon Gaming Co. @ 2018</h4>
+</footer>
 </body>
 </html>
